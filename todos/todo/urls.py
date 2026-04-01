130e87b6423de4c42b58d63c80e6aff42ba682f0
u.kadam <u.kadam@easternenterprise.com>
2026-04-01 16:47:30 +0530

    2.1.2: TemplateView

--- todos/todo/urls.py
+++ todos/todo/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-
+from django.views.generic.base import TemplateView # view class for class based views(2.1.1)
 
 urlpatterns = [
     #path("", views.home, name="home"),
@@ -9,4 +9,8 @@ urlpatterns = [
     # that can be used in the URL patterns.
     path("", views.HomeView.as_view(), name="home"),
     path("add/", views.AddView.as_view(), name="add_todo"),
+
+    # path("about/", TemplateView.as_view(template_name="todo/about.html"  ), name="about"),
+    path("about/", views.AboutView.as_view(), name="about"),
+    path("about/<str:name>/", views.AboutView.as_view(), name="about_name"),
 ]
--- todos/todo/views.py
+++ todos/todo/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import View # view class for class based views(2.1.1)
+from django.views.generic import TemplateView # (2.1.2)
 from .forms import TodoForm
 from todo.models import Todo
 from django.shortcuts import redirect
@@ -30,4 +31,14 @@ class AddView(View):
             return redirect("home")
         
         return render(request, "todo/add.html", {"form": form})
+
+class AboutView(TemplateView):
+    template_name = "todo/about.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        print(kwargs)
+        context["message"] = f"This is the about page for {kwargs['name']}. Message from the AboutView class. Class extends TemplateView and overrides the get_context_data method to add a message to the context that will be passed to the template. The template can then display this message using the {{ message }} variable."
+        return context
+
     
