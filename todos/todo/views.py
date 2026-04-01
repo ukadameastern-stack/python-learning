dc6e767cfa764198790d80a454899f6a2e7f08a7
u.kadam <u.kadam@easternenterprise.com>
2026-04-01 18:43:47 +0530

    2.2.2.1: FormView

--- todos/todo/views.py
+++ todos/todo/views.py
@@ -3,9 +3,8 @@ from django.http import HttpResponse
 from django.views import View # view class for class based views(2.1.1)
 from django.views.generic import TemplateView # (2.1.2)
 from django.views.generic import RedirectView # (2.1.3)
-from django.views.generic import ListView # (2.1.1.1)
-from django.views.generic import DetailView # (2.1.1.2)
-from django.views.generic import DetailView # (2.1.1.2)
+from django.views.generic import ListView # (2.2.1.1)
+from django.views.generic import DetailView # (2.2.1.2)
 from django.views.generic import FormView # (2.2.2.1)
 from .forms import TodoForm
 from todo.models import Todo
@@ -47,16 +46,16 @@ class HomeView(ListView):
     #     # We can also use other filters like startswith, endswith, exact, etc.
      
     
-class AddView(View):
-    def get(self, request):
-        form = TodoForm()
-        return render(request, "todo/add.html", {"form": form})    
+# class AddView(View):
+#     def get(self, request):
+#         form = TodoForm()
+#         return render(request, "todo/add.html", {"form": form})    
     
-    def post(self, request):
-        form = TodoForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect("home")
+#     def post(self, request):
+#         form = TodoForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             return redirect("home")
         
 #         return render(request, "todo/add.html", {"form": form})
 
