820e6559a67c56d01fb106b30ca5fb8be3552fcf
u.kadam <u.kadam@easternenterprise.com>
2026-04-01 17:36:44 +0530

    2.2.1.1: ListView

--- todos/todo/views.py
+++ todos/todo/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django.views import View # view class for class based views(2.1.1)
 from django.views.generic import TemplateView # (2.1.2)
 from django.views.generic import RedirectView # (2.1.3)
+from django.views.generic import ListView # (2.1.1.1)
 from .forms import TodoForm
 from todo.models import Todo
 from django.shortcuts import redirect
@@ -14,11 +15,34 @@ def home(request):
     return HttpResponse("Hello, World!")
 
 # Class based view (2.1.1)
-class HomeView(View):
-    def get(self, request):
-        todos = Todo.objects.all()
-        #return HttpResponse("Hello, World! This is a class-based view.")
-        return render(request, "todo/index.html", {"todos": todos})
+# class HomeView(View):
+#     def get(self, request):
+#         todos = Todo.objects.all()
+#         #return HttpResponse("Hello, World! This is a class-based view.")
+#         return render(request, "todo/index.html", {"todos": todos})
+
+class HomeView(ListView):
+    model = Todo
+    ordering = ["-id"] # This will order the todos by id in descending order. 
+    # The latest added todo will be shown at the top of the list. 
+    # If we want to order it in ascending order, we can use "id" instead of "-id".
+
+    #template_name = "todo/index.html" # By default, 
+    # ListView will look for a template named "todo/todo_list.html" 
+    # (app_name/model_name_list.html). 
+    # If we want to use a different template name, 
+    # we can specify it using the template_name attribute.
+
+    #context_object_name = "todos" # This is the name of the variable 
+    # that will be used in the template to access the list of todos.
+    # By default, it would be "object_list", but we are changing it 
+    # to "todos" for better readability in the template.
+
+    # def get_queryset(self):
+    #     return Todo.objects.filter(todo__icontains="buy")
+    #     # This will filter the todos that contain the word "buy" in the todo field.
+    #     # We can also use other filters like startswith, endswith, exact, etc.
+     
     
 class AddView(View):
     def get(self, request):
